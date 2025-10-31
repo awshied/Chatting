7e878833f51136ff7a97abd8cd93d5707713c1d5
unknown <vendettaktl@gmail.com>
2025-10-31 21:15:37 +0700

    database set up & shitty signup config

---
+++ Backend/src/controllers/auth.controller.js
@@ -0,0 +1,61 @@
+import { generateToken } from "../lib/utils.js";
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+
+export const signup = async (req, res) => {
+  const { fullName, email, password } = req.body;
+
+  try {
+    if (!fullName || !email || !password) {
+      return res.status(400).json({ message: "Isi semua kolomnya yaaa 💖" });
+    }
+
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Passwordnya harus minimal ada 6 karakter ☠️" });
+    }
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      return res
+        .status(400)
+        .json({ message: "Sayang sekali, emailnya ga valid ☹️" });
+    }
+
+    const user = await User.findOne({ email });
+    if (user)
+      return res
+        .status(400)
+        .json({ message: "Email sudah terpakai, coba pikirin email lain 😭" });
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      fullName,
+      email,
+      password: hashedPassword,
+    });
+
+    if (newUser) {
+      // generateToken(newUser._id, res);
+      // await newUser.save();
+
+      const savedUser = await newUser.save();
+      generateToken(savedUser._id, res);
+
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        email: newUser.email,
+        profilePic: newUser.profilePic,
+      });
+    } else {
+      res.status(400).json({ message: "Duhh! Datamu ga valid ⚔️" });
+    }
+  } catch (error) {
+    console.log("Wah, ga bisa signup dulu:", error);
+    res.status(500).json({ message: "Servernya lagi error 🗿" });
+  }
+};
